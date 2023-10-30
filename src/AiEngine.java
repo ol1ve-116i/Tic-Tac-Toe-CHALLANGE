4914aa2aa3fe1b61f30ac8edda985ff7a3c7bea7
AntKinton <antvidgui@alu.edu.gva.es>
2023-10-30 20:54:51 +0100

    Planteando posibles métodos para AiEngine

--- src/AiEngine.java
+++ src/AiEngine.java
@@ -1,2 +1,29 @@
 public class AiEngine {
+
+    private int row, col;
+
+
+    public AiEngine() {
+
+    }
+
+    public AiEngine(Board board) {
+
+    }
+
+
+    public boolean checkWin() {
+
+    }
+
+
+
+    public void blockPlayer() {
+
+    }
+
+
+    public void chooseCenter() {
+
+    }
 }
