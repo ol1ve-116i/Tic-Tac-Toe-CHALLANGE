5a6e708a3eb81f10c7efc75c3dddba90b32173da
ol1ve-116i <luadeodio@alu.edu.gva.es>
2023-10-31 18:18:20 +0100

    Dia 1

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,32 @@
 public class Player {
+    private int shoot;
+    private int p1;
+    private int p2;
+    private int currentPlayer = p1;
+
+    private boolean gameOver = false;
+
+    public Player(int shoot, int currentPlayer, int p1, int p2, boolean gameOver) {
+        this.shoot = shoot;
+        this.currentPlayer = currentPlayer;
+        this.gameOver = gameOver;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public int getShoot() {
+
+        return shoot;
+    }
+
+    private int getCurrentPlayer() {
+        if (currentPlayer == p1) {
+            currentPlayer = p2;
+        } else {
+            currentPlayer = p1;
+        }
+        return currentPlayer;
+    }
+
+
 }
