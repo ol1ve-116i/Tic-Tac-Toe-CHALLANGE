7523ca1373d3fb75d3c6bec3dc9f0216fb7f9ef6
AntKinton <antvidgui@alu.edu.gva.es>
2023-11-02 20:47:59 +0100

    AI's seen the light..

--- src/AiEngine.java
+++ src/AiEngine.java
@@ -1,19 +1,51 @@
 public class AiEngine {
 
-    private char mark;
+    private String playerName;
+    private char symbol;
 
+    private boolean ai;
 
-    public AiEngine(char mark) {
-        this.mark = mark;
+    public AiEngine(String playerName,char symbol, Boolean ai) {
+        this.playerName = playerName;
+        this.symbol = symbol;
+        this.ai = ai;
+    }
 
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public boolean isAi() {
+        return ai;
     }
 
 
-    public boolean checkWin(Board board, Player) {
-        for (int i = 0; i < board.; i++){
-            for (int i = 0; i < logncol; j++) {
+    public char[] goForWin(Board board, AiEngine aiPlay) {
+        boolean winnable = false;
+        char[] winPos = new char[2];
+        //searching for winnable row
+        while ()
+            for (int col = 0; col < board.LENGTH; col++) {
+                for (int )
+                if (board.getCell(row,0) == aiPlay.symbol || board.getCell(row, 1) == aiPlay.symbol
+                        || board.getCell(row, 2) == aiPlay.symbol) {
+
+                    board.getCell(row,col);
+                    break;
+
+
+                }
+                for (int col = 0; col < board.LENGTH; col++) {
+
+
+
+
                 if (board.getCasilla() = VACIA o ESTA LLENA o de que jugador es);
-                matrix[i][j]
+                matrix[i][j]*/
             }
         }
     }
@@ -29,7 +61,7 @@ public class AiEngine {
 
     }
 
-    public void ramdomMark() {
+    public void randomMove() {
 
     }
 }
